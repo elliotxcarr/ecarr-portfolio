4dbce98bfdf92c45f97a8b96d749078f30ffbea9
elliotxcarr <1902552@rgu.ac.uk>
2023-07-19 14:46:50 +0100

    adding dist

--- src/component/LineDiv.jsx
+++
@@ -1,7 +0,0 @@
-
-const LineDiv =({ width = "w-full", color})=>{
-
-    return <div className={`h-2 ${width} bg-${color} z-50`}/>
-}
-
-export default LineDiv;
--- src/component/skillsList.jsx
+++
@@ -1,4 +0,0 @@
-
-let skillsList = ["Javascript", "HTML", "CSS", "Tailwind", "React", "React Native", "Node.js", "Unity Dev in C#", "Blender", "Adobe Creative Suite"]
-
-export default skillsList
